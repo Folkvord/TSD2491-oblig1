701ec95b6d367027b82fc1a2107566190c64f2f1
FOLKVORD <folkvord.kristoffer@gmail.com>
2025-04-05 15:55:09 +0200

    Navigasjon til User Registration

---
+++ TSD2491-oblig1-260562/Controllers/UserRegistrationController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace TSD2491_oblig1_260562.Controllers;
+
+public class UserRegistrationController : Controller{
+
+    public IActionResult Index(){
+        return View();
+    }
+
+    
+
+}
