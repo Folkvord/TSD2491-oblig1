7aa81f3c72a72feed58c36685a98b763a8a94c6b
FOLKVORD <folkvord.kristoffer@gmail.com>
2025-04-05 15:35:13 +0200

    kontrollerklasse

---
+++ TSD2491-oblig1-260562/Controllers/UserRegisterController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers;
+
+public class UserRegisterController : Controller{
+
+    public String Index(){
+        return "LOL";
+    }
+
+}
